0ea8ac08c9b64c4500d2cc29a55b251aab1228d8
productdevbook <38668796+productdevbook@users.noreply.github.com>
2021-11-01 06:42:58 +0300

    core: unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 import { defineNuxtConfig } from 'nuxt3'
+import UnoCSS from 'unocss/vite'
+import UnocssIcons from '@unocss/preset-icons'
+
 
 export default defineNuxtConfig({
+    css: [
+        '~/styles/main.css',
+    ],
+    plugins: [
+        '~/plugins/uno.ts',
+    ],
+    vite: {
+        build: {},
+        plugins: [
+            UnoCSS({
+                presets: [
+                    UnocssIcons(),
+                ],
+            }),
+        ],
+    },
 })
