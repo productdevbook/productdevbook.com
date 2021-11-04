c4aa314107c1ad0a99df7f8dbed57eb483283172
productdevbook <38668796+productdevbook@users.noreply.github.com>
2021-11-04 09:31:50 +0300

    feat: add pages

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="max-w-1xl mx-auto bg-gray-200 p-4 rounded-lg">
+        <div class="flex">
+            <figure class="h-20 w-20 m-0">
+                <img
+                    src="https://nuxtjs.org/design-kit/colored-logo.png"
+                    height="80"
+                    width="80"
+                    alt="logo"
+                />
+            </figure>
+            <div class="flex-col">
+                <p class="font-bold">Title</p>
+                <p
+                    class="font-normal text-pink-600"
+                >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis delectus consectetur voluptatem dolorum pariatur a ipsa repellat tenetur magni! Fugit?</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+
+    },
+})
+</script>
