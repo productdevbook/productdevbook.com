83b51481e471b64b13bc44e1fe479cb65a57bb40
productdevbook <38668796+productdevbook@users.noreply.github.com>
2021-11-05 20:20:57 +0300

    feat: component frameworks

---
+++ components/frameworks.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div v-if="loading">loading....</div>
+        <div v-else-if="result" class="flex space-x-4">
+            <span
+                v-for="item in result.framework"
+                :key="item?.id"
+                class="bg-gray-200 py-2 px-3 rounded-md text-gray-900 font-medium"
+            >
+                <!-- <img :src="item." alt=""> -->
+                {{ item?.title }}
+            </span>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useFrameworksQuery } from '~/types/types'
+
+
+export default defineComponent({
+    setup() {
+        const { result, loading } = useFrameworksQuery()
+        console.log(result.value)
+        return {
+            result, loading
+        }
+    },
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,14 @@
 <template>
     <div class="mx-auto max-w-[600px] space-y-10">
         <div class="mt-10">
-            <div v-if="loading">loading....</div>
-            <div v-else="result" class="flex space-x-4">
-                <span
-                    v-for="item in result.framework"
-                    :key="item?.id"
-                    class="bg-gray-200 py-2 px-3 rounded-md text-gray-900 font-medium"
-                >
-                    <!-- <img :src="item." alt=""> -->
-                    {{ item?.title }}
-                </span>
-            </div>
+            <Suspense>
+                <template #default>
+                    <Frameworks />
+                </template>
+                <template #fallback>
+                    <div>loading...</div>
+                </template>
+            </Suspense>
         </div>
         <div class="max-w-1xl mx-auto bg-gray-200 p-4 rounded-lg">
             <div class="flex">
