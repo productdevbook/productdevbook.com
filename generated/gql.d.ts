62a577c6464f108c924f6d6d2dfa3d6ebc611490
productdevbook <38668796+productdevbook@users.noreply.github.com>
2021-12-21 19:30:17 +0300

    feat: vue 3 + vite

--- generated/gql.d.ts
+++
@@ -1,9 +0,0 @@
-
-declare module '*/frameworks.graphql' {
-  import { DocumentNode } from 'graphql';
-  const defaultDocument: DocumentNode;
-  export const frameworks: DocumentNode;
-
-  export default defaultDocument;
-}
-    
