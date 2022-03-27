7a8ddac0ab22492f5086a8b620284f1d05a29a06
productdevbook <hi@productdevbook.com>
2022-03-27 22:05:40 +0300

    yarn 2 node-modules add and vite config change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    vite: {
+        logLevel: "info",
+        optimizeDeps: {
+            include: [
+                "vue",
+                "ufo",
+            ]
+        }
+    }
 })
