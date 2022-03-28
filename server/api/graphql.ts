35e959a21cb8734d3a7c18e15d6d651e5789e759
productdevbook <hi@productdevbook.com>
2022-03-28 12:32:16 +0300

    fix: graphql.ts

--- server/api/graphql.ts
+++ server/api/graphql.ts
@@ -1,6 +1,6 @@
-import type { IncomingMessage, ServerResponse } from 'http'
-import { ApolloServer, gql } from 'apollo-server-micro'
-import Cors from 'micro-cors'
+import type { IncomingMessage, ServerResponse } from "http";
+import { ApolloServer, gql } from "apollo-server-micro";
+import Cors from "micro-cors";
 const typeDefs = gql`
   type Query {
     sayHello: String
@@ -10,45 +10,46 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     sayHello(parent, args, context) {
-      return 'Hello World!';
+      return "Hello World!";
     },
   },
 };
 
-const cors = Cors()
-
+const cors = Cors();
 
 const whitelist = [
-  'localhost:3000',
-  'http://localhost:3000',
-  'https://studio.apollographql.com',
-  'https://productdevbook.com',
-  'http://127.0.0.1',
-  'http://localhost:3000/api/graphql',
+  "localhost:3000",
+  "http://localhost:3000",
+  "https://studio.apollographql.com",
+  "https://productdevbook.com",
+  "http://127.0.0.1",
+  "http://localhost:3000/api/graphql",
   process.env.CORS_ORIGIN,
 ];
 
-
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 const startServer = apolloServer.start();
-export default cors(async (req: IncomingMessage, res:ServerResponse) => {
+export default cors(async (req: IncomingMessage, res: ServerResponse) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.host);
   if (checkURL(req)) {
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
+    if (req.method === "OPTIONS") {
+      res.end();
+      return;
+    }
     await startServer;
     await await apolloServer.createHandler({
-      path: '/',
-    })(req, res)
+      path: "/",
+    })(req, res);
   } else {
-    res.end()
+    res.end();
   }
 });
 
 function checkURL(req: IncomingMessage) {
-  const reqOrigin = req.headers.origin
-  console.log(reqOrigin)
+  const reqOrigin = req.headers.host;
   if (!reqOrigin || whitelist.indexOf(reqOrigin) !== -1) {
-    return true
+    return true;
   } else {
-    new Error('Not allowed by CORS')
+    new Error("Not allowed by CORS");
   }
 }
