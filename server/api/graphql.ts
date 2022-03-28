2cfaa63187f4711b2a4227e549d4155e2b42c037
productdevbook <hi@productdevbook.com>
2022-03-28 17:30:42 +0300

    Update graphql.ts

--- server/api/graphql.ts
+++ server/api/graphql.ts
@@ -22,6 +22,8 @@ const whitelist = [
   "http://localhost:3000",
   "https://studio.apollographql.com",
   "https://productdevbook.com",
+  "productdevbook.com",
+  "studio.apollographql.com",
   "http://127.0.0.1",
   "http://localhost:3000/api/graphql",
   process.env.CORS_ORIGIN,
@@ -30,12 +32,13 @@ const whitelist = [
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 const startServer = apolloServer.start();
 export default cors(async (req: IncomingMessage, res: ServerResponse) => {
+  console.log(req.headers.host)
   res.setHeader("Access-Control-Allow-Origin", req.headers.host);
   if (checkURL(req)) {
-    // if (req.method === "OPTIONS") {
-    //   res.end();
-    //   return;
-    // }
+    if (req.method === "OPTIONS") {
+      res.end();
+      return;
+    }
     await startServer;
     await await apolloServer.createHandler({
       path: "/",
@@ -47,9 +50,7 @@ export default cors(async (req: IncomingMessage, res: ServerResponse) => {
 
 function checkURL(req: IncomingMessage) {
   const reqOrigin = req.headers.host;
-  console.log(`${reqOrigin} is allowed`);
-  console.log(req.headers);
-  console.log(req);
+  console.log(whitelist.indexOf(reqOrigin))
   if (!reqOrigin || whitelist.indexOf(reqOrigin) !== -1) {
     return true;
   } else {
