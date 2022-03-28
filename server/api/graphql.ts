efcbbca55a2688e21da327ec0485df46ffdd1376
productdevbook <hi@productdevbook.com>
2022-03-28 12:50:38 +0300

    Update graphql.ts

--- server/api/graphql.ts
+++ server/api/graphql.ts
@@ -32,10 +32,10 @@ const startServer = apolloServer.start();
 export default cors(async (req: IncomingMessage, res: ServerResponse) => {
   res.setHeader("Access-Control-Allow-Origin", req.headers.host);
   if (checkURL(req)) {
-    // if (req.method === "OPTIONS") {
-    //   res.end();
-    //   return;
-    // }
+    if (req.method === "OPTIONS") {
+      res.end();
+      return;
+    }
     await startServer;
     await await apolloServer.createHandler({
       path: "/",
@@ -47,9 +47,10 @@ export default cors(async (req: IncomingMessage, res: ServerResponse) => {
 
 function checkURL(req: IncomingMessage) {
   const reqOrigin = req.headers.host;
+  console.log(`${reqOrigin} is allowed`);
+  console.log(req.headers);
+  console.log(req);
   if (!reqOrigin || whitelist.indexOf(reqOrigin) !== -1) {
-    console.log(`${reqOrigin} is allowed`);
-    console.log(req.headers);
     return true;
   } else {
     new Error("Not allowed by CORS");
