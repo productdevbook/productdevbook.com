cd39161404cb6b2fc90a180edeca9db7909aa127
productdevbook <hi@productdevbook.com>
2022-03-29 19:17:37 +0300

    core: @graphql-yoga/node

--- server/api/graphql.ts
+++ server/api/graphql.ts
@@ -1,63 +1,25 @@
 import type { IncomingMessage, ServerResponse } from "http";
-import { ApolloServer, gql } from "apollo-server-micro";
-import Cors from "micro-cors";
-const typeDefs = gql`
-  type Query {
-    sayHello: String
-  }
-`;
+import { createServer } from '@graphql-yoga/node'
 
-const resolvers = {
-  Query: {
-    sayHello(parent, args, context) {
-      return "Hello World!";
-    },
+const server = createServer<{
+  req: IncomingMessage
+  res: ServerResponse
+}>({
+  schema: {
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => 'Hello Hello Hello'
+      }
+    }
   },
-};
-const cors = Cors({});
+  cors: false,
+  endpoint: '/api/graphql',
+})
 
-const whitelist = [
-  "localhost:3000",
-  "https://studio.apollographql.com",
-  "https://productdevbook.com",
-  "productdevbook.com",
-  "studio.apollographql.com",
-  "http://127.0.0.1",
-  process.env.CORS_ORIGIN,
-];
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
-const startServer = apolloServer.start();
-export default cors(async (req: IncomingMessage, res: ServerResponse) => {
-  const reqOrigin = req.headers.origin || req.headers.host;
-  console.log(reqOrigin, 'reqOrigin');
-  if (!checkURL(req, reqOrigin)) {
-    res.end("You are not allowed to access");
-  }
-  //@ts-ignore
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    reqOrigin
-  );
-  console.log(res.getHeaders(), 'res.getHeaders()');
-
-  if (req.method === "OPTIONS") {
-    res.end();
-    return;
-  }
-  await startServer;
-  await await apolloServer.createHandler({
-    path: "/",
-  })(req, res);
-});
-function checkURL(req: IncomingMessage, reqOrigin: string): boolean {
-
-  console.log(reqOrigin);
-  if (reqOrigin && whitelist.indexOf(reqOrigin) !== -1) {
-    console.log(reqOrigin, "is allowed");
-    return true;
-  } else {
-    new Error("Not allowed by CORS");
-  }
-}
+export default server
