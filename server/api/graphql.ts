2ed16704da307ed37998a11602c26eb949ee2db1
productdevbook <hi@productdevbook.com>
2022-03-29 07:21:56 +0300

    Update graphql.ts

--- server/api/graphql.ts
+++ server/api/graphql.ts
@@ -14,44 +14,44 @@ const resolvers = {
     },
   },
 };
-// @TODO add cors
-// const cors = Cors({origin});
+const cors = Cors({});
 
-// const whitelist = [
-//   "localhost:3000",
-//   "http://localhost:3000",
-//   "https://studio.apollographql.com",
-//   "https://productdevbook.com",
-//   "productdevbook.com",
-//   "studio.apollographql.com",
-//   "http://127.0.0.1",
-//   "http://localhost:3000/api/graphql",
-//   process.env.CORS_ORIGIN,
-// ];
+const whitelist = [
+  "localhost:3000",
+  "https://studio.apollographql.com",
+  "https://productdevbook.com",
+  "productdevbook.com",
+  "studio.apollographql.com",
+  "http://127.0.0.1",
+  process.env.CORS_ORIGIN,
+];
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 const startServer = apolloServer.start();
-export default async (req: IncomingMessage, res: ServerResponse) => {
-  console.log(req.headers.host)
-  res.setHeader("Access-Control-Allow-Origin", req.headers.host);
-  
+export default cors(async (req: IncomingMessage, res: ServerResponse) => {
+  checkURL(req);
     if (req.method === "OPTIONS") {
       res.end();
       return;
     }
-    await startServer;
-    await await apolloServer.createHandler({
-      path: "/",
-    })(req, res);
-
+    if (checkURL(req)) {
+      //@ts-ignore
+      res.setHeader("Access-Control-Allow-Credentials", true);
+      res.setHeader("Access-Control-Allow-Origin", req.headers.origin || req.headers.host);
+      console.log(res.getHeaders())
+      await startServer;
+      await await apolloServer.createHandler({
+        path: "/",
+      })(req, res);
+    } else {
+      res.end('You are not allowed to access');
+    }
+})
+function checkURL(req: IncomingMessage): boolean {
+  const reqOrigin = req.headers.origin || req.headers.host;
+  if (reqOrigin && whitelist.indexOf(reqOrigin) !== -1) {
+    return true;
+  } else {
+    new Error("Not allowed by CORS");
+  }
 }
-
-// function checkURL(req: IncomingMessage) {
-//   const reqOrigin = req.headers.host;
-//   console.log(whitelist.indexOf(reqOrigin))
-//   if (!reqOrigin || whitelist.indexOf(reqOrigin) !== -1) {
-//     return true;
-//   } else {
-//     new Error("Not allowed by CORS");
-//   }
-// }
