2d9512f3792529fbe28ab3c6b4c8c6f3baa763b4
productdevbook <hi@productdevbook.com>
2022-03-29 07:21:38 +0300

    Update graphql.ts

--- server/api/graphql.ts
+++ server/api/graphql.ts
@@ -14,16 +14,15 @@ const resolvers = {
     },
   },
 };
-const cors = Cors({origin});
+const cors = Cors({});
 
 const whitelist = [
-  "http://localhost:3000",
+  "localhost:3000",
   "https://studio.apollographql.com",
   "https://productdevbook.com",
   "productdevbook.com",
   "studio.apollographql.com",
   "http://127.0.0.1",
-  "http://localhost:3000/api/graphql",
   process.env.CORS_ORIGIN,
 ];
 
@@ -31,21 +30,26 @@ const apolloServer = new ApolloServer({ typeDefs, resolvers });
 const startServer = apolloServer.start();
 export default cors(async (req: IncomingMessage, res: ServerResponse) => {
   checkURL(req);
-  //@ts-ignore
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || '');
     if (req.method === "OPTIONS") {
       res.end();
       return;
     }
-    await startServer;
-    await await apolloServer.createHandler({
-      path: "/",
-    })(req, res);
+    if (checkURL(req)) {
+      //@ts-ignore
+      res.setHeader("Access-Control-Allow-Credentials", true);
+      res.setHeader("Access-Control-Allow-Origin", req.headers.origin || req.headers.host);
+      console.log(res.getHeaders())
+      await startServer;
+      await await apolloServer.createHandler({
+        path: "/",
+      })(req, res);
+    } else {
+      res.end('You are not allowed to access');
+    }
 })
-function checkURL(req: IncomingMessage) {
-  const reqOrigin = req.headers.origin || '';
-  if (!reqOrigin || whitelist.indexOf(reqOrigin) !== -1) {
+function checkURL(req: IncomingMessage): boolean {
+  const reqOrigin = req.headers.origin || req.headers.host;
+  if (reqOrigin && whitelist.indexOf(reqOrigin) !== -1) {
     return true;
   } else {
     new Error("Not allowed by CORS");
